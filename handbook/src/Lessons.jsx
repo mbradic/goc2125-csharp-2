504d4e06ce3b31045639908516d3987169d5ceee
Miroslav Bradic <mbradic@yahoo.com>
2024-05-26 18:36:16 +0200

    markdown from github added

--- handbook/src/Lessons.jsx
+++ handbook/src/Lessons.jsx
@@ -1,15 +1,30 @@
 import course from "./course.json";
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { TopLink } from "./TopLink";
 import { useParams } from "react-router-dom";
+import Markdown from "react-markdown";
 
 function Lessons() {
+  const [markdown, setMarkdown] = useState();
+
   const params = useParams();
   const lesson = course.lessons.find((l) =>
     params.lesson ? l.path === params.lesson : course.lessons[0]
   );
 
-  const topic = params.topic ? lesson.topics.find(t=>t.path===params.topic) : lesson.topics[0]
+  const topic = params.topic
+    ? lesson.topics.find((t) => t.path === params.topic)
+    : lesson.topics[0];
+
+  useEffect(() => {
+    async function getData() {
+      const markdownResponse = await fetch(
+        `https://raw.githubusercontent.com/mbradic/goc2125-csharp-2/main/handbook/public/lessons/${lesson.path}/${topic.path}.md`
+      );
+      setMarkdown(await markdownResponse.text());
+    }
+    getData();
+  }, [lesson.path, topic.path]);
 
   return (
     <>
@@ -23,11 +38,11 @@ function Lessons() {
       </nav>
 
       <hr />
-      
+
       <h2>{lesson.title}</h2>
-      
+
       <hr />
-      
+
       <div style={{ display: "flex" }}>
         <nav style={{ borderRight: "1px solid white", padding: "15px" }}>
           {lesson.topics.map((topic, topicIndex) => (
@@ -40,9 +55,9 @@ function Lessons() {
           ))}
         </nav>
 
-        <div style={{paddingLeft: "15px"}}>
-          <h3>{ topic.title }</h3>
-          <div dangerouslySetInnerHTML={{__html: topic.content}} />
+        <div style={{ paddingLeft: "15px" }}>
+          <h3>{topic.title}</h3>
+          <Markdown>{markdown}</Markdown>
         </div>
       </div>
     </>
